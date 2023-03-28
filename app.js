5050ab40d6007108836a750d1f351f35181f53b8
Natanael <natanael.santana@outlook.com.br>
2023-03-27 23:07:58 -0300

    mongo database added to store every post the user makes in the blog, also, mongoose included on the project

--- app.js
+++ app.js
@@ -3,8 +3,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose")
 const _ = require('lodash')
 
+
+// Just dump content to put in the pages
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -17,14 +20,25 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-const postItem = []
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB")
+
+const itemsSchema = { 
+  name: String,
+  content: String
+}
+
+const Item = mongoose.model('Item', itemsSchema)
+
 
 
 app.get("/", function (req, res){
+  Item.find( {})
+      .then (function (items) {
   res.render("home", {
     homeStartingContent,
-    postItem
+    items:items,
   })
+})
 })
 
 app.get("/about", function (req, res){
@@ -45,43 +59,42 @@ app.get("/compose", function (req, res){
   res.render("compose", {
   })
 })
-
+/*this method takes information from a form in compose.ejs file
+and turn possible to insert this into a database*/
 app.post("/compose", function (req, res) {
-  let itemTitle = req.body.newItemTitle
-  let item = req.body.newItem
-  let composeItems = {
-    itemTitle,
-    item
-  }
   
-    postItem.push(composeItems)
-    res.redirect("/")
+  const item = new Item ({
+    name:  req.body.newItemTitle,
+    content: req.body.newItem
+  })
+    item.save()
+    res.redirect("/");
    
 })
 
+
 app.get("/post/:topic", function (req, res){
-  let topic = _.lowerCase(req.params.topic)
- 
-
-
-  postItem.forEach(function(post){
-    let title = _.lowerCase(post.itemTitle)
-    let content = post.item
-    let articleTitle = post.itemTitle
-    
-    if (title === topic){
+  let topic = _.lowerCase(req.params.topic);
+  
+/*this method turn possible to take information from databse
+and pass to post.ejs file*/
+  Item.find( {})
+      .then (function (items) {
+    items.forEach(function(posts){
+      let title = _.lowerCase(posts.name)
+      let content = posts.content
+      let articleTitle = posts.name
+      
+      if (title === topic){
         res.render("post", {
-      content,
-      articleTitle
-      })
-    } else {
-      console.log("Azedou")
-    }
+        content,
+        articleTitle
+        })
+     } 
   })
- 
+})
 })
 
-
-app.listen(3001, function () {
-  console.log("Servers started on port 3001")
+app.listen(3000, function () {
+  console.log("Servers started on port 3000")
 })
